c791f8f7f5c9afd63ff6be3cce17cd6b4bf9cd95
andy135793726 <andy135793726@gmail.com>
2013-10-10 02:07:16 +0800

    [server部分]登入可廣播、同步使用者清單
    [server]
    
    增加應付要求使用者清單(C_ASK_USERLIST)的功能
    應該還有一些小地方ㄏㄏ不過忘了

--- ChatServer/ChatServer/handleClient.cs
+++ ChatServer/ChatServer/handleClient.cs
@@ -38,17 +38,36 @@ namespace ChatServer
 
                         switch(Program.parseMsg(ref data))
                         {
+                            case MsgType.C_ASK_USERLIST:
+                                //debug
+                                Console.WriteLine("get C_ASK_USERLIST");
+                                
+                                
+                                Program.broadcastList(0);
+
+                                break;
+
                             case MsgType.C_MSG_TO_BCGROUP:
                                 string dataString = System.Text.Encoding.ASCII.GetString(data);
+
+                                //debug
+                                Console.WriteLine("dataString:" + dataString);
+
                             //only chatting
                                 string[] commands = dataString.Split(spCh);
                                 //dataString = dataString.Substring(dataString.IndexOf('\x01')+1);
                             //int len = dataString.Substring(0,dataString.IndexOf("$"));
                                 //dataString = dataString.Substring(0,dataString.IndexOf('\x01'));
+
+                                int groupNum = int.Parse(commands[0]);
+                                string message = commands[1];
                                 
+
                                 Console.WriteLine("From " + clName + " - data: " + commands[0]);
-                                Program.broadcastChat(commands[1], clName,int.Parse(commands[0]), true);
-                            break;
+                                Program.broadcastChat(message, clName, groupNum, true);
+                                break;
+
+                            
                             
                         }
                     }
@@ -60,12 +79,13 @@ namespace ChatServer
                 }
                 catch(Exception e)
                 {
+                    Console.WriteLine("error: " + e.ToString());
+
                     Console.WriteLine(clName + " closed");
                     Program.clientList.Remove(clName);
                     for (int i = 0; i < 20; i++ )
                         Program.BCGroupList[i].Remove(clName);
                     Program.broadcastChat(clName + " has left.", clName,0, false);
-                    //Console.WriteLine(e.ToString());
                     return;
                 }
             }
